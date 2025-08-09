3b33b159e486716d07f5adf30ae746d52c3003da
gusprojects008 <gus0100@tutamail.com>
2025-08-08 22:13:54 -0300

    init module

---
+++ terminal_colors/terminal_colors.py
@@ -0,0 +1,29 @@
+class Colors:
+      def __init__(self, color, text):
+          self.red = "\033[31m"
+          self.green = "\033[32m"
+          self.blue = "\033[34m"
+          self.cyan = "\033[36m"
+          self.purple = "\033[35m"
+          self.reset = "\033[0m"
+          self.pink = "\033[95m"
+
+          self.bright = '\033[1m'
+          self.background_green = '\033[42m'
+          self.background_red = '\033[41m'
+          self.blink = '\033[5m'
+          self.sublime = '\033[4m'
+
+          self.bg = f'{self.bright}{self.green}'
+          self.br = f'{self.bright}{self.red}'
+          self.bb = f'{self.bright}{self.blue}'
+
+          self.ok = f"{self.bg}[OK]{self.reset} "
+          self.check = f"{self.purple}[CHECK]{self.reset} "
+          self.warning = f"{self.background_red}[WARN]{self.reset} "
+          self.error = f"{self.br}[ERROR]{self.reset} "
+          self.finalization = f"{self.bb}[END]{self.reset} "
+
+      def __call__(self, color_flag, text):
+          code_color = getattr(self, color_flag.lower(), self.reset)
+          return f"{code_color}{text}{self.reset}"
